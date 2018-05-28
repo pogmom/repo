71ab04a2cb36910589ddadc58614463159013d0f
MPG13 <micahplacido@gmail.com>
2018-05-28 14:38:22 -0600

    updates

--- remove.sh
+++ remove.sh
@@ -1,2 +1,2 @@
-rm -r *.deb
-rm -r Packages.bz2
+rm -r ./pkgs/*.deb
+rm -r ./Packages.bz2
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 ./remove.sh
 ./packages.sh
 
-dpkg-scanpackages -m . /dev/null >Packages
+dpkg-scanpackages -m ./pkgs /dev/null >Packages
 bzip2 Packages
